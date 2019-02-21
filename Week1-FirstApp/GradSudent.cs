f9a2519f8f13917d9ec9d85b95709f53f73d5bef
husesims <huseyin.simsek@boun.edu.tr>
2019-02-21 13:45:28 +0300

    asenron örnek

--- Week1-FirstApp/GradSudent.cs
+++ Week1-FirstApp/GradSudent.cs
@@ -2,4 +2,8 @@ public class GradStudent: Student{
     
     public string ThesisTopic { get; set; }
     public bool IsThesisCompleted {get;set;}
+
+    public override bool CanGradute() {
+          return  base.CanGradute()&& IsThesisCompleted;
+    }
 }
--- Week1-FirstApp/Program.cs
+++ Week1-FirstApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace firstapp
 {
@@ -6,7 +8,7 @@ namespace firstapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Cet322 Class!");
+            /*  Console.WriteLine("Hello Cet322 Class!");
             Student.MiniumumGPA=2;
             Course cet322=new Course();
             cet322.Code="CET322";
@@ -33,11 +35,108 @@ namespace firstapp
             huseyin.Courses.Add(huseyinCet301);
             huseyin.Courses.Add(new StudentCourse(math101, Grade.AA));
             huseyin.Courses.Add(new StudentCourse(cet322, Grade.F));
+
+           
+
+            GradStudent mehmet=new GradStudent {
+                 StudentNo ="2013121",
+                 FullName ="Mehmet Şimşek"
+            };
+
+           
+            mehmet.Courses.Add(new StudentCourse(math101, Grade.AA));
+            mehmet.Courses.Add(new StudentCourse(cet322, Grade.BA));
+
+      GradStudent gs2;
+        Student s2;
+
+        s2=mehmet;
+       
+        gs2=huseyin;
+        
+
+
+            Print(mehmet);
+            Print(huseyin);
+            */
+            var starTime=DateTime.Now;
+            Calculate1();
+            Calculate2();
+            Calculate3();
+            var sure=DateTime.Now-starTime;
+            Console.WriteLine("Toplam ms:" + sure.TotalSeconds);
+
+           Calculator().GetAwaiter().GetResult();
             
-            Console.WriteLine(huseyin.CalculateGPA());
-
-            Console.WriteLine(huseyin.CanGradute());
-
         }
+        public static async Task Calculator() {
+            var starTime2=DateTime.Now;
+
+            Task Calc1=Calculate1Async();
+            Task Calc2=Calculate2Async();
+            Task<int> Calc3=Calculate3Async();
+
+            await Calc1;
+            await Calc2;
+            await Calc3;
+            /*
+            await Calculate1Async();
+            await Calculate2Async();
+            await Calculate3Async();
+            
+            var sure2=DateTime.Now-starTime2;
+            Console.WriteLine("Toplam ms:" + sure2.TotalSeconds);
+*/
+        }
+
+        public static void Print(Student student) {
+            Console.WriteLine($" Adı :{student.FullName} GPA : {student.CalculateGPA()} Meznuniyet : {student.CanGradute()}");
+        }
+
+        public static void Calculate1() {
+              Console.WriteLine("Calculate 1 Start " + DateTime.Now.ToLongTimeString());
+            Thread.Sleep(2000);
+            Console.WriteLine("Calculate 1 End " + DateTime.Now.ToLongTimeString());
+        }
+
+         public static void Calculate2() {
+               Console.WriteLine("Calculate 2 Start " + DateTime.Now.ToLongTimeString());
+           Thread.Sleep(3000);
+               Console.WriteLine("Calculate 2 End " + DateTime.Now.ToLongTimeString());
+    
+        }
+
+         public static int Calculate3() {
+               Console.WriteLine("Calculate 3 Start " + DateTime.Now.ToLongTimeString());
+
+           Thread.Sleep(1000);
+               Console.WriteLine("Calculate 3 End " + DateTime.Now.ToLongTimeString());
+           
+            return 3;
+        }
+
+         public static async Task Calculate1Async() {
+               Console.WriteLine("Calculate 1 Start " + DateTime.Now.ToLongTimeString());
+
+             await Task.Delay(2000);
+               Console.WriteLine("Calculate 1 End " + DateTime.Now.ToLongTimeString());
+          
+        }
+
+         public static async Task Calculate2Async() {
+                 Console.WriteLine("Calculate 2 Start " + DateTime.Now.ToLongTimeString());
+             await Task.Delay(3000);
+               Console.WriteLine("Calculate 2 End " + DateTime.Now.ToLongTimeString());          
+        }
+       
+
+        public static async Task<int> Calculate3Async() {
+                Console.WriteLine("Calculate 3 Start " + DateTime.Now.ToLongTimeString());
+             await Task.Delay(1000);
+            Console.WriteLine("Calculate 3 End "  + DateTime.Now.ToLongTimeString());
+            return 3;
+        }
+       
+
     }
 }
